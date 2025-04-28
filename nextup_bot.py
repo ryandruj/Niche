392a0fe90bde5803cb6c7d1e3adc5dcb565f07df
Ryan Drujon <ryandrujon@macbookair.myfiosgateway.com>
2025-04-28 02:18:11 -0400

    Fix: listen to all messages

--- nextup_bot.py
+++ nextup_bot.py
@@ -160,8 +160,10 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
 
 def main():
     app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+
     app.add_handler(CommandHandler('start', start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    app.add_handler(MessageHandler(filters.ALL, handle_message))
+
     app.run_polling()
 
 if __name__ == '__main__':
